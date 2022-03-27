130454c9df39c1de66d540836a0c3943d8e6ef7f
ClayBenson94 <contact@claybenson.me>
2022-03-27 19:06:47 -0400

    Initial game list (client ID and auth token left out until we figure out config values for firebase functions)
    Co-authored-by: Nick S <nkstokowski@users.noreply.github.com>

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require("firebase-functions");
 const cors = require('cors')({origin: true});
+const axios = require('axios').default;
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
   cors(request, response, () => {
@@ -8,4 +9,22 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
       "message": "Hello from Firebase!"
     }});
   });
+});
+
+exports.getGameList = functions.https.onRequest(async (request, response) => {
+  const resp = await axios({
+    method: 'post',
+    url: 'https://api.igdb.com/v4/games',
+    data: 'fields name,franchises.*; where name = "Super Mario Bros. 2"; limit 1;',
+    headers: {
+      'Client-ID': 'TODO',
+      'Authorization': 'Bearer TODO'
+    },
+  });
+
+  console.log(resp);
+
+  response.send({
+    data: resp.data
+  })
 });
