4e0f5ffc625120742d8de067fe056f57c6028c5c
ClayBenson94 <contact@claybenson.me>
2022-03-27 15:59:38 -0400

    Infra updates to have firebase emulators work for local development

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,11 @@
 const functions = require("firebase-functions");
+const cors = require('cors')({origin: true});
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+  cors(request, response, () => {
+    functions.logger.info("Hello logs!", {structuredData: true});
+    response.send({"data": {
+      "message": "Hello from Firebase!"
+    }});
+  });
 });
--- src/index.js
+++ src/index.js
@@ -4,6 +4,39 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFunctions, httpsCallable, connectFunctionsEmulator } from "firebase/functions";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA5qwPfBc76YcMK8a7IPtzGCaOZxfwN5OE",
+  authDomain: "gameguessr-app.firebaseapp.com",
+  projectId: "gameguessr-app",
+  storageBucket: "gameguessr-app.appspot.com",
+  messagingSenderId: "972045674167",
+  appId: "1:972045674167:web:921a12f48e7e6c39b60f32"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const functions = getFunctions(app);
+
+if (process.env.REACT_APP_USE_FIREBASE_EMULATORS === "true") {
+  console.log("Using local Firebase emulators");
+  connectFunctionsEmulator(functions, "localhost", 5001);
+}
+
+const helloWorld = httpsCallable(functions, "helloWorld");
+
+helloWorld().then(result => {
+  console.log("PROCESS", process.env);
+  console.log("RESULT DATA MESSAGE", result.data.message)
+})
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
